e589f8200c9bb8075f94adc698410724a739a413
aiden <allaher@icloud.com>
2024-03-28 17:06:41 +0800

    Create schema.py

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/schema.py
@@ -0,0 +1,20 @@
+from drf_spectacular.extensions import OpenApiSerializerExtension
+from drf_spectacular.utils import Direction
+from drfexts.serializers.serializers import WCCModelSerializer
+
+
+class DynamicFieldsModelSerializerExtension(OpenApiSerializerExtension):
+    """
+    See issue: https://github.com/tfranzel/drf-spectacular/issues/375
+    """
+    target_class = WCCModelSerializer  # this can also be an import string
+    match_subclasses = True
+    priority = -1
+
+    def map_serializer(self, auto_schema: "AutoSchema", direction: Direction):  # noqa: F821
+        return auto_schema._map_serializer(self.target, direction, bypass_extensions=True)
+
+    def get_name(self, auto_schema, direction):
+        # FIXME API-DOC 报错
+        # return self.target.ref_name
+        return getattr(self.target, "ref_name", self.target.__class__.__name__)
